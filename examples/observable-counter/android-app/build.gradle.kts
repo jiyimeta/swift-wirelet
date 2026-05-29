eb6ade4c4568c1fc51445da7b9df620fe4ace37e
Kiichi Ito <jiyi.meta@gmail.com>
2026-05-29 16:16:36 +0900

    feat(observable-counter): scaffold Android Gradle project + wrapper

---
+++ examples/observable-counter/android-app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("io.github.jiyimeta.wirelet") version "0.0.1-local" apply false
+}
